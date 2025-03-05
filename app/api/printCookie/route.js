809a44bf8bdfbb10c1f61897a7d0ebc72c55b35e
Nelson Matias <nellson.maia@gmail.com>
2025-03-05 12:19:13 +0000

    adding refresh session endpoint

--- app/api/printCookie/route.js
+++ app/api/printCookie/route.js
@@ -7,7 +7,6 @@ export async function GET(request) {
 
   // Require a proof of possession header.
   const proof = request.headers.get("Sec-Session-Response");
-  console.log("DEBUG all headers:", request.headers);
   console.log("DEBUG: Proof of possession header:", proof);
 //   if (!proof) {
 //     console.log("ERROR: Missing proof of possession header.");
